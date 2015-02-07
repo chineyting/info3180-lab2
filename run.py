25915b4ac1a7b5fc634614c8721935b2ce5b9159
Simantha Hong <simorah@gmail.com>
2015-02-07 01:49:26 +0000

    customized my app

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
 #! /usr/bin/env python
+import os
+from flask import render_template
+from flask import Flask
 from app import app
+import time
+
+@app.route('/profile/')
+def profile():
+  return render_template('profile.html', now = timeinfo())
+
+def timeinfo():
+  now = time.strftime("%a, %d %b %Y")
+  return now
+
 app.run(debug=True,host="0.0.0.0",port=8080)
